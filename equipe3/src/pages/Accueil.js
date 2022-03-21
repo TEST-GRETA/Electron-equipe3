73433427f29ae15e29c9f1dc8f44d2d6ca90f405
Gaelle21 <gaelle.olanda21@gmail.com>
2022-03-21 17:09:45 +0100

    Modal authentification

--- equipe3/src/pages/Accueil.js
+++ equipe3/src/pages/Accueil.js
@@ -1,14 +1,25 @@
 import "../styles/accueil.css"
 import {Link} from "react-router-dom";
 import users from "../datas/users.json";
-
+import Modal from 'react-modal';
+import React from 'react';
+import Authentification from "./Authentification";
 
 export default function Accueil() {
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    function openModal() {
+        setIsOpen(true);
+      }
+
+    
+      function closeModal() {
+        setIsOpen(false);
+      }
     return (
         
         <div>
             <div className="svgHaut">
-                <Link to ="/authentification" className="link">Connexion</Link>
+                <p to ="/authentification" className="link" onClick={openModal}>Connexion</p>
                 <svg width="625" height="128" viewBox="0 0 625 128" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M354.331 99.7243C469.234 167.851 752.287 94.5015 1051.59 42.9681L1112.65 -126.69C833.314 -149.375 232.789 -197.524 65.3898 -208.641C-143.86 -222.537 210.703 14.5656 354.331 99.7243Z" fill="url(#paint0_linear_3_233)"/>
                 <defs>
@@ -31,6 +42,13 @@ export default function Accueil() {
                 </div>
             )}
             </div>
+            <div className="containerModal">
+                <Modal className="modal"
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Example Modal"
+                ><Authentification/></Modal>
+            </div>
             <div className="svg">
                 <svg  width="757" height="102" viewBox="0 0 757 102" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M415 33.8034C303 -38.9966 17.1667 22.6368 -284 61.8034L-352 228.803C-73.8333 262.97 524.2 335.803 691 353.803C899.5 376.303 555 124.803 415 33.8034Z" fill="url(#paint0_linear_3_232)"/>
@@ -49,3 +67,4 @@ export default function Accueil() {
             </div>
     );
   }
+
